f2a1bfa6e0bf782e81f432810ad105955946b317
nguyen.danimal@gmail.com <nguyen.danimal@gmail.com>
2022-07-11 13:07:27 -0500

    added project type to schema from data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,12 @@
 const { projects, clients } = require('../sampleData.js');
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema } = require('graphql');
+const { 
+    GraphQLObjectType,
+    GraphQLID, 
+    GraphQLString, 
+    GraphQLSchema, 
+    GraphQLList 
+} = require('graphql');
 
 // Client Type
 const ClientType = new GraphQLObjectType({
@@ -13,9 +19,39 @@ const ClientType = new GraphQLObjectType({
     })
 });
 
+// Project Type
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString }
+    })
+});
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parentValue, args) {
+                return projects;
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: { id: { type: GraphQLID } },
+            resolve(parentValue, args) {
+                return projects.find(project => project.id === args.id);
+            }
+        },
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parentValue, args) {
+                return clients;
+            }
+        },
         client: {
             type: ClientType,
             args: { id: { type: GraphQLID } },
