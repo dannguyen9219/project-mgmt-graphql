139f5a352426f61401297ce9bacf5900a1b09c24
nguyen.danimal@gmail.com <nguyen.danimal@gmail.com>
2022-07-12 12:38:28 -0500

    delete client mutation, and apollo cache

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,31 @@ import Header from "./components/Header";
 import Clients from "./components/Clients";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        }
+      }
+    }
+  }
+});
+// Adding a merge cache function to update client/projects list when deleting a client/project instead of re-fetching data. Then call cache in client function below
+
+
 const client = new ApolloClient({
   uri: 'http://localhost:8000/graphql',
-  cache: new InMemoryCache()
+  cache,
 });
 
 function App() {
